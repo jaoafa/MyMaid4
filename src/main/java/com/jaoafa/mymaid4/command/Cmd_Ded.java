868ff5dafcf119b208d3f1fa2174973477da7618
Tomachi <lego@aol.jp>
2021-03-20 00:14:13 +0900

    fix: 気になるところをいくつか修正3

--- src/main/java/com/jaoafa/mymaid4/command/Cmd_Ded.java
+++ src/main/java/com/jaoafa/mymaid4/command/Cmd_Ded.java
@@ -38,7 +38,7 @@ public class Cmd_Ded extends MyMaidLibrary implements CommandPremise {
         if (player.getGameMode() == GameMode.SURVIVAL || player.getGameMode() == GameMode.ADVENTURE) {
             SendMessage(player, details(), "Dedコマンドはサバイバル・アドベンチャーモードでは利用できません。");
             SendMessage(player, details(), "クリエイティブモードに切り替えてから実行してください。");
-            SendMessage(player, details(), ChatColor.RED + "" + ChatColor.BOLD + "===[!]警告===");
+            SendMessage(player, details(), ChatColor.RED + "" + ChatColor.BOLD + "=== [!] 警告 ===");
             SendMessage(player, details(), ChatColor.RED + "" + ChatColor.BOLD + "PvP等での「/ded」コマンドの利用は原則禁止です！");
             SendMessage(player, details(), ChatColor.RED + "" + ChatColor.BOLD + "多く使用すると迷惑行為として認識される場合もあります！");
 
@@ -51,7 +51,7 @@ public class Cmd_Ded extends MyMaidLibrary implements CommandPremise {
             player.teleport(location);
             String locationDescription = String.format("( X:%s Y:%s Z:%s )", location.getBlockX(), location.getBlockY(), location.getBlockZ());
             SendMessage(player, details(), "最終死亡場所" + ChatColor.BOLD + locationDescription + ChatColor.RESET + "にテレポートしました。");
-            SendMessage(player, details(), ChatColor.RED + "" + ChatColor.BOLD + "===[!]警告===");
+            SendMessage(player, details(), ChatColor.RED + "" + ChatColor.BOLD + "=== [!] 警告 ===");
             SendMessage(player, details(), ChatColor.RED + "" + ChatColor.BOLD + "PvP等での「/ded」コマンドの利用は原則禁止です！");
             SendMessage(player, details(), ChatColor.RED + "" + ChatColor.BOLD + "多く使用すると迷惑行為として認識される場合もあります！");
         }
