251cc0990290123ae4b948ad4d72f7f37ab5cae8
yuuaHP <53546237+yuuahp@users.noreply.github.com>
2021-03-22 16:04:12 +0900

    feat: loginmsg一部実装

--- src/main/java/com/jaoafa/mymaid4/event/Event_SSKColor.java
+++ src/main/java/com/jaoafa/mymaid4/event/Event_SSKColor.java
@@ -2,10 +2,12 @@ package com.jaoafa.mymaid4.event;
 
 import com.jaoafa.mymaid4.lib.MyMaidLibrary;
 import com.jaoafa.mymaid4.lib.SKKColorManager;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class Event_SSKColor extends MyMaidLibrary implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
@@ -17,5 +19,18 @@ public class Event_SSKColor extends MyMaidLibrary implements Listener {
                 event.getFormat()
             )
         );
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onEvent_JoinChangeMessage(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        String JoinMessage = SKKColorManager.getPlayerSKKJoinMessage(player);
+        if (JoinMessage != null) {
+            event.setJoinMessage(JoinMessage);
+        }
+
+        /*for(Player p: Bukkit.getServer().getOnlinePlayers()) {
+            new TabListSKKReloader(p).runTaskLater(plugin, 20L);
+        }*/
     }
 }
