941c4b3f150d5055512a104b0e59e4ab79ee755e
yuuaHP <53546237+yuuahp@users.noreply.github.com>
2021-03-27 08:03:54 +0900

    add: chattest

---
+++ src/main/java/com/jaoafa/mymaid4/command/Cmd_Chat.java
@@ -0,0 +1,66 @@
+/*
+ * jaoLicense
+ *
+ * Copyright (c) 2021 jao Minecraft Server
+ *
+ * The following license applies to this project: jaoLicense
+ *
+ * Japanese: https://github.com/jaoafa/jao-Minecraft-Server/blob/master/jaoLICENSE.md
+ * English: https://github.com/jaoafa/jao-Minecraft-Server/blob/master/jaoLICENSE-en.md
+ */
+
+package com.jaoafa.mymaid4.command;
+
+import cloud.commandframework.Command;
+import cloud.commandframework.context.CommandContext;
+import cloud.commandframework.meta.CommandMeta;
+import com.jaoafa.mymaid4.lib.CommandPremise;
+import com.jaoafa.mymaid4.lib.MyMaidCommand;
+import com.jaoafa.mymaid4.lib.MyMaidLibrary;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Cmd_Chat extends MyMaidLibrary implements CommandPremise {
+    @Override
+    public MyMaidCommand.Detail details() {
+        return new MyMaidCommand.Detail(
+            "ded",
+            "最後に死亡した場所にテレポートします。"
+        );
+    }
+
+    @Override
+    public MyMaidCommand.Cmd register(Command.Builder<CommandSender> builder) {
+        return new MyMaidCommand.Cmd(
+            builder
+                .meta(CommandMeta.DESCRIPTION, "最後に死亡した場所にテレポートします。")
+                .senderType(Player.class)
+                .handler(this::chatFake)
+                .build()
+        );
+    }
+
+    void chatFake(CommandContext<CommandSender> context) {
+        Player player = (Player) context.getSender();
+        SendMessage(player, details(), context.toString());
+        /*context.getOrDefault(0)
+        ChatColor color = ChatColor.GRAY;
+        List<String> colors = Arrays.stream(args).filter(
+            arg -> arg != null && arg.startsWith("color:")).collect(Collectors.toList());
+        if (colors.size() != 0) {
+            for (ChatColor cc : ChatColor.values()) {
+                if (!cc.name().equalsIgnoreCase(colors.get(0).substring("color:".length()))) {
+                    continue;
+                }
+                color = cc;
+            }
+        }
+        List<String> texts = Arrays.stream(Arrays.copyOfRange(args, 1, args.length)).filter(
+            arg -> arg != null && !arg.startsWith("color:")).collect(Collectors.toList());
+        String text = ChatColor.translateAlternateColorCodes('&', String.join(" ", texts));
+        if (args[0].equalsIgnoreCase("jaotan")) {
+            color = ChatColor.GOLD;
+        }
+        chatFake(color, args[0], text);*/
+    }
+}
