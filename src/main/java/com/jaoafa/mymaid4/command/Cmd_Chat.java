2131bfdd2504004d90a6fada57b1764311f3bf47
yuuaHP <yuuahp@icloud.com>
2021-03-27 17:39:05 +0900

    feat: /chatを実装

--- src/main/java/com/jaoafa/mymaid4/command/Cmd_Chat.java
+++ src/main/java/com/jaoafa/mymaid4/command/Cmd_Chat.java
@@ -21,11 +21,14 @@ import com.jaoafa.mymaid4.lib.MyMaidLibrary;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Cmd_Chat extends MyMaidLibrary implements CommandPremise {
+
     @Override
     public MyMaidCommand.Detail details() {
         return new MyMaidCommand.Detail(
@@ -34,36 +37,118 @@ public class Cmd_Chat extends MyMaidLibrary implements CommandPremise {
         );
     }
 
-    List<String> colors = new ArrayList<>();
     @Override
     public MyMaidCommand.Cmd register(Command.Builder<CommandSender> builder) {
-        colors.add(ChatColor.BLUE.toString());
-        colors.add(ChatColor.RED.toString());
-        colors.add(ChatColor.GREEN.toString());
-        colors.add(ChatColor.WHITE.toString());
-        colors.add(ChatColor.DARK_BLUE.toString());
-        colors.add(ChatColor.DARK_PURPLE.toString());
-        colors.add(ChatColor.LIGHT_PURPLE.toString());
-        colors.add(ChatColor.AQUA.toString());
-        colors.add(ChatColor.DARK_AQUA.toString());
-        colors.add(ChatColor.DARK_GRAY.toString());
-        colors.add(ChatColor.DARK_GREEN.toString());
-        colors.add(ChatColor.DARK_RED.toString());
-        colors.add(ChatColor.BLACK.toString());
-        colors.add(ChatColor.GOLD.toString());
+
         return new MyMaidCommand.Cmd(
             builder
                 .meta(CommandMeta.DESCRIPTION, "偽のプレイヤーに喋らせます。")
-                .argument(StringArgument.greedy("text"))
+                .argument(StringArgument.<CommandSender>newBuilder("text")
+                    .greedy().withSuggestionsProvider(this::colors))
                 .senderType(Player.class)
                 .handler(this::chatFake)
                 .build()
         );
     }
 
+    private List<String> colors(@NonNull CommandContext<CommandSender> commandSenderCommandContext, @NonNull String s) {
+        ArrayList<String> colors = new ArrayList<>();
+        colors.add("color:blue");
+        colors.add("color:red");
+        colors.add("color:green");
+        colors.add("color:white");
+        colors.add("color:dark_blue");
+        colors.add("color:dark_purple");
+        colors.add("color:light_purple");
+        colors.add("color:aqua");
+        colors.add("color:dark_aqua");
+        colors.add("color:dark_gray");
+        colors.add("color:dark_green");
+        colors.add("color:dark_red");
+        colors.add("color:black");
+        colors.add("color:gold");
+        colors.add("color:yellow");
+        return colors;
+    }
+
+
     void chatFake(CommandContext<CommandSender> context) {
-        Player player = (Player) context.getSender();
-        SendMessage(player, details(), context.getOrDefault("text", ""));
+
+        final String[] msg = {context.getOrDefault("text", "")};
+
+        //color:をchatcolorとして認識
+        final ChatColor[] chatColor = new ChatColor[1];
+        Arrays.stream(msg[0].split(" ")).forEach(s -> {
+            if (s.startsWith("color:")) {
+                if (s.equals("color:blue")) {
+                    chatColor[0] = ChatColor.BLUE;
+                }
+                if (s.equals("color:red")) {
+                    chatColor[0] = ChatColor.RED;
+                }
+                if (s.equals("color:green")) {
+                    chatColor[0] = ChatColor.GREEN;
+                }
+                if (s.equals("color:white")) {
+                    chatColor[0] = ChatColor.WHITE;
+                }
+                if (s.equals("color:dark_blue")) {
+                    chatColor[0] = ChatColor.DARK_BLUE;
+                }
+                if (s.equals("color:dark_purple")) {
+                    chatColor[0] = ChatColor.DARK_PURPLE;
+                }
+                if (s.equals("color:light_purple")) {
+                    chatColor[0] = ChatColor.LIGHT_PURPLE;
+                }
+                if (s.equals("color:aqua")) {
+                    chatColor[0] = ChatColor.AQUA;
+                }
+                if (s.equals("color:dark_aqua")) {
+                    chatColor[0] = ChatColor.DARK_AQUA;
+                }
+                if (s.equals("color:dark_gray")) {
+                    chatColor[0] = ChatColor.DARK_GRAY;
+                }
+                if (s.equals("color:dark_green")) {
+                    chatColor[0] = ChatColor.DARK_GREEN;
+                }
+                if (s.equals("color:dark_red")) {
+                    chatColor[0] = ChatColor.DARK_RED;
+                }
+                if (s.equals("color:black")) {
+                    chatColor[0] = ChatColor.BLACK;
+                }
+                if (s.equals("color:gold")) {
+                    chatColor[0] = ChatColor.GOLD;
+                }
+                if (s.equals("color:yellow")) {
+                    chatColor[0] = ChatColor.YELLOW;
+                }
+
+                msg[0] = msg[0].replaceFirst(s, "");
+                msg[0] = msg[0].trim();
+            }
+        });
+
+        //colorを抜いた最初の引数をfakePlayerをして認識
+        String fakePlayer = msg[0].split(" ")[0];
+
+        msg[0] = msg[0].replaceFirst(fakePlayer, "");
+        msg[0] = msg[0].trim();
+
+        //color null check
+        if (chatColor[0] == null) {
+            chatColor[0] = ChatColor.GRAY;
+        }
+        //player jaotan check
+        if (fakePlayer.equals("jaotan")) {
+            chatColor[0] = ChatColor.GOLD;
+        }
+
+        chatFake(chatColor[0], fakePlayer, msg[0]);
+
+
         /*context.getOrDefault(0)
         ChatColor color = ChatColor.GRAY;
         List<String> colors = Arrays.stream(args).filter(
