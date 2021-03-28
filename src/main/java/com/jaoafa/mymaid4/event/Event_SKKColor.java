2216a30ca3439ffdaf20dd0c20b7cfa339dfa852
yuuaHP <yuuahp@icloud.com>
2021-03-28 19:22:08 +0900

    fix: いろいろ

--- src/main/java/com/jaoafa/mymaid4/event/Event_SKKColor.java
+++ src/main/java/com/jaoafa/mymaid4/event/Event_SKKColor.java
@@ -13,6 +13,8 @@ package com.jaoafa.mymaid4.event;
 
 import com.jaoafa.mymaid4.lib.MyMaidLibrary;
 import com.jaoafa.mymaid4.lib.SKKColorManager;
+import io.papermc.paper.chat.ChatComposer;
+import io.papermc.paper.event.player.AsyncChatEvent;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -26,10 +28,13 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class Event_SKKColor extends MyMaidLibrary implements Listener {
     // TODO 非推奨イベントのため変更する必要がありますが、使い方が分からないので困り果てています
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onEvent_ChatSKK(AsyncPlayerChatEvent event) {
-        event.setFormat(
-            event.getFormat().replaceFirst("%1\\$s",String.format("%s■%s%s",SKKColorManager.getPlayerChatColor(event.getPlayer()), ChatColor.WHITE,event.getPlayer().getName()))
-        );
+    public void onEvent_ChatSKK(AsyncChatEvent event) {
+        Player player = event.getPlayer();
+        ChatComposer composer = (_player, displayName, message) -> Component.translatable("chat.type.text", Component.text().append(
+            Component.text("■", SKKColorManager.getPlayerColor(player)),
+            displayName
+        ), message);
+        event.composer(composer);
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
