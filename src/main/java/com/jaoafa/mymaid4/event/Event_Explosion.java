2ba2f6ccdf63ec97d6701f1f46db2cdb344c5ecc
ぞぞかす <jojohounds@gmail.com>
2021-03-30 18:15:16 +0900

    add:event_explosion
    たぶん地図とかが破損しなくなるやつ

---
+++ src/main/java/com/jaoafa/mymaid4/event/Event_Explosion.java
@@ -0,0 +1,28 @@
+/*
+ * jaoLicense
+ *
+ * Copyright (c) 2021 jao Minecraft Server
+ *
+ * The following license applies to this project: jaoLicense
+ *
+ * Japanese: https://github.com/jaoafa/jao-Minecraft-Server/blob/master/jaoLICENSE.md
+ * English: https://github.com/jaoafa/jao-Minecraft-Server/blob/master/jaoLICENSE-en.md
+ */
+
+package com.jaoafa.mymaid4.event;
+
+import com.jaoafa.mymaid4.lib.MyMaidLibrary;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.hanging.HangingBreakEvent;
+import org.bukkit.event.hanging.HangingBreakEvent.RemoveCause;
+
+public class Event_Explosion extends MyMaidLibrary implements Listener {
+    @EventHandler
+    public void onHangingDamageByTNT(HangingBreakEvent event) {
+        if (event.getCause() != RemoveCause.EXPLOSION) {
+            return;
+        }
+        event.setCancelled(true);
+    }
+}
