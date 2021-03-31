d6af7e4127a3310fab091c49f1a6dc6d032f696a
yuuaHP <yuuahp@icloud.com>
2021-03-31 18:19:57 +0900

    feat: LoginSuccessful

---
+++ src/main/java/com/jaoafa/mymaid4/event/Event_LoginSuccessful.java
@@ -0,0 +1,58 @@
+/*
+ * jaoLicense
+ *
+ * Copyright (c) 2021 jao Minecraft Server
+ *
+ * The following license applies to this project: jaoLicense
+ *
+ * Japanese: https://github.com/jaoafa/jao-Minecraft-Server/blob/master/jaoLICENSE.md
+ * English: https://github.com/jaoafa/jao-Minecraft-Server/blob/master/jaoLICENSE-en.md
+ */
+
+package com.jaoafa.mymaid4.event;
+
+import com.jaoafa.mymaid4.Main;
+import com.jaoafa.mymaid4.lib.MyMaidData;
+import com.jaoafa.mymaid4.lib.MyMaidLibrary;
+import com.jaoafa.mymaid4.lib.MySQLDBManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.UUID;
+
+public class Event_LoginSuccessful extends MyMaidLibrary implements Listener {
+    @EventHandler(priority = EventPriority.MONITOR,
+                  ignoreCancelled = true)
+    public void OnJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        UUID uuid = player.getUniqueId();
+
+        new BukkitRunnable() {
+            public void run() {
+                MySQLDBManager MySQLDBManager = MyMaidData.getMainMySQLDBManager();
+                if (MySQLDBManager == null) {
+                    return;
+                }
+                try {
+                    Connection conn = MySQLDBManager.getConnection();
+                    PreparedStatement statement = conn.prepareStatement(
+                        "UPDATE login SET login_success = ? WHERE uuid = ? ORDER BY id DESC LIMIT 1");
+                    statement.setBoolean(1, true);
+                    statement.setString(2, uuid.toString());
+                    statement.executeUpdate();
+                    statement.close();
+                } catch (SQLException e) {
+                    reportError(getClass(), e);
+                    return;
+                }
+            }
+        }.runTaskAsynchronously(Main.getJavaPlugin());
+    }
+}
