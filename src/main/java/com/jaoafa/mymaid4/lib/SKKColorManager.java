74cb614db18fc8f4cddf5bf09e9b5d8c050a0599
Tomachi [ICHIGO] <lego@aol.jp>
2021-04-02 08:10:01 +0900

    fix: 四角色がうまく表示されない不具合

--- src/main/java/com/jaoafa/mymaid4/lib/SKKColorManager.java
+++ src/main/java/com/jaoafa/mymaid4/lib/SKKColorManager.java
@@ -14,15 +14,15 @@ package com.jaoafa.mymaid4.lib;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Team;
 
-import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 
 public class SKKColorManager {
-    static List<NamedTextColor> ChatColors = Arrays.asList(
+    static List<NamedTextColor> TextColors = Arrays.asList(
         NamedTextColor.GRAY,
         NamedTextColor.WHITE,
         NamedTextColor.DARK_BLUE,
@@ -38,6 +38,22 @@ public class SKKColorManager {
         NamedTextColor.DARK_PURPLE,
         NamedTextColor.LIGHT_PURPLE);
 
+    static List<ChatColor> ChatColors = Arrays.asList(
+        ChatColor.GRAY,
+        ChatColor.WHITE,
+        ChatColor.DARK_BLUE,
+        ChatColor.BLUE,
+        ChatColor.AQUA,
+        ChatColor.DARK_AQUA,
+        ChatColor.DARK_GREEN,
+        ChatColor.GREEN,
+        ChatColor.YELLOW,
+        ChatColor.GOLD,
+        ChatColor.RED,
+        ChatColor.DARK_RED,
+        ChatColor.DARK_PURPLE,
+        ChatColor.LIGHT_PURPLE);
+
     static int getVoteCount(Player player) {
         PlayerVoteDataMono pvd = new PlayerVoteDataMono(player);
 
@@ -46,8 +62,21 @@ public class SKKColorManager {
 
     static NamedTextColor getCustomColor(Player player) {
         PlayerVoteDataMono pvd = new PlayerVoteDataMono(player);
+        String color = pvd.getCustomColor();
+        if (color == null) return null;
 
-        return MyMaidLibrary.getNamedTextColor(pvd.getCustomColor());
+        return MyMaidLibrary.getNamedTextColor(color);
+    }
+
+    static ChatColor getCustomChatColor(Player player) {
+        PlayerVoteDataMono pvd = new PlayerVoteDataMono(player);
+        String color = pvd.getCustomColor();
+        if (color == null) return null;
+        try {
+            return ChatColor.valueOf(color);
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
     }
 
     /**
@@ -59,9 +88,24 @@ public class SKKColorManager {
      */
     public static NamedTextColor getPlayerColor(Player player) {
         int count = getVoteCount(player);
-        NamedTextColor color = ChatColors.get(calculateRank(count));
+        NamedTextColor color = TextColors.get(calculateRank(count));
         NamedTextColor customColor = getCustomColor(player);
-        if(customColor != null) color = customColor;
+        if (customColor != null) color = customColor;
+        return color;
+    }
+
+    /**
+     * プレイヤーの四角色を取得する
+     *
+     * @param player プレイヤー
+     *
+     * @return 四角色
+     */
+    public static ChatColor getPlayerChatColor(Player player) {
+        int count = getVoteCount(player);
+        ChatColor color = ChatColors.get(calculateRank(count));
+        ChatColor customColor = getCustomChatColor(player);
+        if (customColor != null) color = customColor;
         return color;
     }
 
