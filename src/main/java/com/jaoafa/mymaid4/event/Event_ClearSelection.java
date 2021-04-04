4c5cf73b6e135c7f3bca147c81cf369493d43bcb
yuuaHP <yuuahp@icloud.com>
2021-04-04 11:58:39 +0900

    feat: selclick

---
+++ src/main/java/com/jaoafa/mymaid4/event/Event_ClearSelection.java
@@ -0,0 +1,51 @@
+/*
+ * jaoLicense
+ *
+ * Copyright (c) 2021 jao Minecraft Server
+ *
+ * The following license applies to this project: jaoLicense
+ *
+ * Japanese: https://github.com/jaoafa/jao-Minecraft-Server/blob/master/jaoLICENSE.md
+ * English: https://github.com/jaoafa/jao-Minecraft-Server/blob/master/jaoLICENSE-en.md
+ */
+
+package com.jaoafa.mymaid4.event;
+
+import com.jaoafa.mymaid4.lib.MyMaidLibrary;
+import com.jaoafa.mymaid4.lib.SelClickManager;
+import org.bukkit.GameMode;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class Event_ClearSelection extends MyMaidLibrary implements Listener {
+    @EventHandler
+    public void onAirClick(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        if (event.getAction() != Action.LEFT_CLICK_AIR) {
+            return;
+        }
+        if (event.getPlayer().getGameMode() != GameMode.CREATIVE) {
+            return;
+        }
+        if (!isAMRV(player)) {
+            return;
+        }
+        if (!player.isSprinting()) {
+            return;
+        }
+        if (!SelClickManager.isEnable(player)) {
+            return;
+        }
+        player.performCommand("/sel");
+        player.getWorld().playSound(
+            player.getLocation(),
+            Sound.BLOCK_WOODEN_BUTTON_CLICK_ON,
+            3,
+            1
+        );
+    }
+}
---
+++ src/main/java/com/jaoafa/mymaid4/lib/SelClickManager.java
@@ -0,0 +1,54 @@
+/*
+ * jaoLicense
+ *
+ * Copyright (c) 2021 jao Minecraft Server
+ *
+ * The following license applies to this project: jaoLicense
+ *
+ * Japanese: https://github.com/jaoafa/jao-Minecraft-Server/blob/master/jaoLICENSE.md
+ * English: https://github.com/jaoafa/jao-Minecraft-Server/blob/master/jaoLICENSE-en.md
+ */
+
+package com.jaoafa.mymaid4.lib;
+
+import com.jaoafa.mymaid4.Main;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class SelClickManager {
+    static Map<UUID, Boolean> cache = new HashMap<>();
+    static File file = new File(Main.getJavaPlugin().getDataFolder(), "selclick.yml");
+
+    public static boolean isEnable(Player player) {
+        if (cache.containsKey(player.getUniqueId())) {
+            return cache.get(player.getUniqueId());
+        } else {
+            FileConfiguration conf = YamlConfiguration.loadConfiguration(file);
+            if (conf.contains(player.getUniqueId().toString())) {
+                cache.put(player.getUniqueId(), conf.getBoolean(player.getUniqueId().toString()));
+                return conf.getBoolean(player.getUniqueId().toString());
+            } else {
+                cache.put(player.getUniqueId(), false);
+                return false;
+            }
+        }
+    }
+
+    public static void setStatus(Player player, boolean newValue) {
+        cache.put(player.getUniqueId(), newValue);
+        FileConfiguration conf = YamlConfiguration.loadConfiguration(file);
+        conf.set(player.getUniqueId().toString(), newValue);
+        try {
+            conf.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
