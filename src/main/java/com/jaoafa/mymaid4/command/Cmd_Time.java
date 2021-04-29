5dc66d54ec6264cd098e865d94efdebc42a619a2
Tomachi <lego@aol.jp>
2021-04-29 11:13:50 +0900

    fix: format code

--- src/main/java/com/jaoafa/mymaid4/command/Cmd_Time.java
+++ src/main/java/com/jaoafa/mymaid4/command/Cmd_Time.java
@@ -56,6 +56,7 @@ public class Cmd_Time extends MyMaidLibrary implements CommandPremise {
                 .build()
         );
     }
+
     void timeSetByName(CommandContext<CommandSender> context) {
         Player player = (Player) context.getSender();
         String timeName = context.get("timeName"); //day or 1000 どれが入ってるかわからない
@@ -63,25 +64,25 @@ public class Cmd_Time extends MyMaidLibrary implements CommandPremise {
         int timeInt = 0; //1000とかで指定された場合に代入
         boolean getTimeByNumber; //数値指定かどうか
 
-        try{
+        try {
             //とりあえずパースしてみる。成功したら数値指定をtrueに
             timeInt = Integer.parseInt(timeName);
             getTimeByNumber = true;
-        } catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             //出来なかったら文字指定なので数値指定はfalse
             getTimeByNumber = false;
         }
 
         //文字指定->数値への変換Map
         Map<String, Integer> timeNameToInt = new HashMap<>();
-        timeNameToInt.put("day",1000);
-        timeNameToInt.put("noon",6000);
-        timeNameToInt.put("night",13000);
-        timeNameToInt.put("midnight",18000);
+        timeNameToInt.put("day", 1000);
+        timeNameToInt.put("noon", 6000);
+        timeNameToInt.put("night", 13000);
+        timeNameToInt.put("midnight", 18000);
 
-        if (!getTimeByNumber&&!timeNameToInt.containsKey(timeName)){
+        if (!getTimeByNumber && !timeNameToInt.containsKey(timeName)) {
             //変換Mapに存在しなかった場合
-            SendMessage(player,details(),"そのような名前の時間は存在しません！");
+            SendMessage(player, details(), "そのような名前の時間は存在しません！");
             return;
         }
 
@@ -91,16 +92,17 @@ public class Cmd_Time extends MyMaidLibrary implements CommandPremise {
             : timeNameToInt.get(timeName);
 
         //設定
-        player.setPlayerTime(resultTime,isRelative);
+        player.setPlayerTime(resultTime, isRelative);
         //お知らせ
-        SendMessage(player,details(), String.format("あなたの時間を%sに設定しました！", resultTime));
+        SendMessage(player, details(), String.format("あなたの時間を%sに設定しました！", resultTime));
     }
+
     void timeAddByInt(CommandContext<CommandSender> context) {
         Player player = (Player) context.getSender();
         int myTimeInt = context.get("timeInt");
         boolean isRelative = context.get("isRelative");
         long myCurrentTime = player.getPlayerTime();
-        player.setPlayerTime(myCurrentTime+myTimeInt,isRelative);
-        SendMessage(player,details(), String.format("あなたの時間を%s進めました！", myTimeInt));
+        player.setPlayerTime(myCurrentTime + myTimeInt, isRelative);
+        SendMessage(player, details(), String.format("あなたの時間を%s進めました！", myTimeInt));
     }
 }
