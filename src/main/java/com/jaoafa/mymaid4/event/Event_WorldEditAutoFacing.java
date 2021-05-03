2e94e7c42e7fda35001096bf4ad31a1693ecc192
yuuaHP <yuuahp@icloud.com>
2021-05-03 13:59:11 +0900

    change: すべてのlookに対応&名称変更

--- src/main/java/com/jaoafa/mymaid4/event/Event_WorldEditAutoFacing.java
+++ src/main/java/com/jaoafa/mymaid4/event/Event_WorldEditAutoFacing.java
@@ -13,32 +13,26 @@ package com.jaoafa.mymaid4.event;
 
 import com.jaoafa.mymaid4.lib.EventPremise;
 import com.jaoafa.mymaid4.lib.MyMaidLibrary;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
-public class Event_SetHandAutoFacing extends MyMaidLibrary implements Listener, EventPremise {
+public class Event_WorldEditAutoFacing extends MyMaidLibrary implements Listener, EventPremise {
     @Override
     public String description() {
-        return "「//set hand[facing=look]」が実行された際に自動で向いている方向に置き換えます。";
+        return "「[facing=look]」が含まれるコマンドが実行された際に自動で向いている方向に置き換えます。";
     }
 
     @EventHandler
     public void onSetHandFacingCommand(PlayerCommandPreprocessEvent event) {
         String command = event.getMessage();
         Player player = event.getPlayer();
-        String[] args = command.split(" ");
-        if (args.length == 0) {
-            return;
-        }
-        if (!command.equalsIgnoreCase("//set hand[facing=look]")) {
+        if (!command.contains("[facing=look]")) {
             return; //set hand[facing=look]コマンド以外
         }
         if (isAMRV(player)) {
-            event.setMessage(command.replace("look",player.getFacing().name()));
+            event.setMessage(command.replace("look", player.getFacing().name()));
         }
     }
 }
