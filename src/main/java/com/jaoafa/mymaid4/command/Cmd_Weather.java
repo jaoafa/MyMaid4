9e9436224deb1022575f17f3fc61c51056956df7
yuuaHP <yuuahp@icloud.com>
2021-05-06 21:11:11 +0900

    feat: weather

---
+++ src/main/java/com/jaoafa/mymaid4/command/Cmd_Weather.java
@@ -0,0 +1,68 @@
+/*
+ * jaoLicense
+ *
+ * Copyright (c) 2021 jao Minecraft Server
+ *
+ * The following license applies to this project: jaoLicense
+ *
+ * Japanese: https://github.com/jaoafa/jao-Minecraft-Server/blob/master/jaoLICENSE.md
+ * English: https://github.com/jaoafa/jao-Minecraft-Server/blob/master/jaoLICENSE-en.md
+ */
+
+package com.jaoafa.mymaid4.command;
+
+import cloud.commandframework.Command;
+import cloud.commandframework.arguments.standard.BooleanArgument;
+import cloud.commandframework.arguments.standard.StringArgument;
+import cloud.commandframework.context.CommandContext;
+import cloud.commandframework.meta.CommandMeta;
+import com.jaoafa.mymaid4.lib.CommandPremise;
+import com.jaoafa.mymaid4.lib.MyMaidCommand;
+import com.jaoafa.mymaid4.lib.MyMaidLibrary;
+import org.bukkit.WeatherType;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Cmd_Weather extends MyMaidLibrary implements CommandPremise {
+    @Override
+    public MyMaidCommand.Detail details() {
+        return new MyMaidCommand.Detail(
+            "weather",
+            "自分だけに適用される天気を設定します。"
+        );
+    }
+
+    @Override
+    public MyMaidCommand.Cmd register(Command.Builder<CommandSender> builder) {
+        return new MyMaidCommand.Cmd(
+            builder
+                .meta(CommandMeta.DESCRIPTION, "自分だけに適用される天気を設定します。")
+                .literal("set")
+                .argument(StringArgument.newBuilder("weatherName")/*.withSuggestionsProvider(this::suggestWeatherName)*/)
+                .senderType(Player.class)
+                .handler(this::weatherSetByName)
+                .build()
+        );
+    }
+
+    void weatherSetByName(CommandContext<CommandSender> context) {
+        Player player = (Player) context.getSender();
+        String weatherName = context.get("weatherName");
+
+        WeatherType weatherType = WeatherType.valueOf(weatherName.toUpperCase());
+        //設定
+        player.setPlayerWeather(weatherType);
+        //お知らせ
+        SendMessage(player, details(), String.format("あなたの天気を%sに設定しました！", weatherType.name()));
+    }
+    /*List<String> suggestWeatherName(final CommandContext<CommandSender> context, final String current) {
+        List<String> list = new ArrayList<>();
+        list.addAll(Arrays.stream(WeatherType.values())
+            .map(Enum::name)
+            .collect(Collectors.toList()));
+
+        return list.stream()
+            .filter(s -> s.toLowerCase().startsWith(current.toLowerCase()))
+            .collect(Collectors.toList());
+    }*/
+}
