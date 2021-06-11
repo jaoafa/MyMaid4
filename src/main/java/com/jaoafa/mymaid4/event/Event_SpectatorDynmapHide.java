fc9f88d2bdcd39298340a62156f8b3c74e828474
Tomachi <lego@aol.jp>
2021-06-12 07:14:55 +0900

    Merge pull request #453 from book000/feature/issue-452-spectator-hide
    feat: #452 スペクテイター時にDynmapから姿を消す

---
+++ src/main/java/com/jaoafa/mymaid4/event/Event_SpectatorDynmapHide.java
@@ -0,0 +1,53 @@
+/*
+ * jaoLicense
+ *
+ * Copyright (c) 2021 jao Minecraft Server
+ *
+ * The following license applies to this project: jaoLicense
+ *
+ * Japanese: https://github.com/jaoafa/jao-Minecraft-Server/blob/master/jaoLICENSE.md
+ * English: https://github.com/jaoafa/jao-Minecraft-Server/blob/master/jaoLICENSE-en.md
+ */
+
+package com.jaoafa.mymaid4.event;
+
+import com.jaoafa.mymaid4.lib.EventPremise;
+import com.jaoafa.mymaid4.lib.MyMaidLibrary;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
+import org.bukkit.plugin.Plugin;
+import org.dynmap.DynmapAPI;
+
+public class Event_SpectatorDynmapHide extends MyMaidLibrary implements Listener, EventPremise {
+    @Override
+    public String description() {
+        return "スペクテイターモードの際、Dynmapから姿を隠します。";
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onChangedGameMode(PlayerGameModeChangeEvent event) {
+        if (!isEnabledPlugin("dynmap")) {
+            return;
+        }
+
+        if (event.getNewGameMode() == GameMode.SPECTATOR) {
+            getDynmapAPI().setPlayerVisiblity(event.getPlayer(), false);
+        } else if (event.getPlayer().getGameMode() == GameMode.SPECTATOR) {
+            getDynmapAPI().setPlayerVisiblity(event.getPlayer(), true);
+        }
+    }
+
+    /**
+     * DynmapAPIを返す
+     *
+     * @return DynmapAPI
+     */
+    DynmapAPI getDynmapAPI() {
+        Plugin dynmap = Bukkit.getPluginManager().getPlugin("dynmap");
+        return (DynmapAPI) dynmap;
+    }
+}
